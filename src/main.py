1de7b0455796fc094c3246ead93e3ee482cf0721
Will Thomas <technimanx@gmail.com>
2020-06-28 21:15:44 +0100

    Add comments to main.py
    I'm sure none of them will be of any use within a week, but it seemed necessary anyway

--- src/main.py
+++ src/main.py
@@ -3,13 +3,16 @@ import tcod
 
 
 def main() -> None:
+    # vars
     screen_width = 80
     screen_height = 50
-    
+
+    # load the tileset
     tileset = tcod.tileset.load_tilesheet(
         "dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
     )
 
+    # set up the context
     with tcod.context.new_terminal(
         screen_width,
         screen_height,
@@ -17,12 +20,18 @@ def main() -> None:
         title="Yet Another Roguelike Tutorial",
         vsync=True
     ) as context:
+        # init console
         root_console = tcod.Console(screen_width, screen_height, order="F")
+
+        # game loop
         while True:
+            # print our player
             root_console.print(x=1, y=1, string="@")
 
+            # present everything we've printed to the screen
             context.present(root_console)
 
+            # accept input
             for event in tcod.event.wait():
                 if event.type == "QUIT":
                     raise SystemExit()
