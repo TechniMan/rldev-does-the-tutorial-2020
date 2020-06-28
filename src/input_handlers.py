9d7336bce454b2013dc7bbe41f0180b6172c5e21
Will Thomas <technimanx@gmail.com>
2020-06-28 21:38:16 +0100

    Add basic input handling and player movement

---
+++ src/input_handlers.py
@@ -0,0 +1,31 @@
+from typing import Optional
+
+import tcod.event
+
+from actions import Action, EscapeAction, MovementAction
+
+
+class EventHandler(tcod.event.EventDispatch[Action]):
+    def ev_quit(self, event: tcod.event.Quit) -> Optional[Action]:
+        raise SystemExit()
+
+    def ev_keydown(self, event: tcod.event.KeyDown) -> Optional[Action]:
+        # if a key isn't pressed, return None by default
+        action: Optional[Action] = None
+
+        # get the `sym`bol pressed by the player
+        key = event.sym
+
+        if key == tcod.event.K_UP:
+            action = MovementAction(dx=0, dy=-1)
+        elif key == tcod.event.K_DOWN:
+            action = MovementAction(dx=0, dy=1)
+        elif key == tcod.event.K_LEFT:
+            action = MovementAction(dx=-1, dy=0)
+        elif key == tcod.event.K_RIGHT:
+            action = MovementAction(dx=1, dy=0)
+        
+        elif key == tcod.event.K_ESCAPE:
+            action = EscapeAction()
+
+        return action
--- src/main.py
+++ src/main.py
@@ -1,16 +1,25 @@
 #!/usr/bin/env python3
 import tcod
 
+from actions import EscapeAction, MovementAction
+from input_handlers import EventHandler
+
 
 def main() -> None:
-    # vars
+    # general vars
     screen_width = 80
     screen_height = 50
+    # player vars
+    player_x = int(screen_width / 2)
+    player_y = int(screen_height / 2)
 
     # load the tileset
     tileset = tcod.tileset.load_tilesheet(
         "dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
     )
+
+    # init event handler
+    event_handler = EventHandler()
 
     # set up the context
     with tcod.context.new_terminal(
@@ -25,16 +34,28 @@ def main() -> None:
 
         # game loop
         while True:
+            # await input from player
+            for event in tcod.event.wait():
+                action = event_handler.dispatch(event)
+
+                if action is None:
+                    continue
+
+                if isinstance(action, MovementAction):
+                    player_x += action.dx
+                    player_y += action.dy
+
+                elif isinstance(action, EscapeAction):
+                    raise SystemExit()
+
+            # clear the console before we print to it
+            root_console.clear()
+
             # print our player
-            root_console.print(x=1, y=1, string="@")
+            root_console.print(x=player_x, y=player_y, string="@")
 
             # present everything we've printed to the screen
             context.present(root_console)
-
-            # accept input
-            for event in tcod.event.wait():
-                if event.type == "QUIT":
-                    raise SystemExit()
 
 
 if __name__ == "__main__":
